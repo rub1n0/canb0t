72c9e717a5175d61844ac8df8769b34c3bc725a5
rub1n0 <rubino.anthony@scrippshealth.org>
2025-08-16 16:14:29 -0700

    Add SD logging for RedBoard CAN sketch

--- canb0t_redboard.ino
+++ canb0t_redboard.ino
@@ -1,12 +1,16 @@
+// CAN logging sketch for SparkFun RedBoard + CAN-Bus Shield
 #include <SPI.h>
+#include <SD.h>
 #include <mcp_can.h>
 
 // Change to true to enable OBD-II PID polling mode
 const bool PID_MODE = false; // false = sniff mode, true = PID mode
 
-// MCP2515 CS pin on SparkFun CAN-Bus Shield
+// MCP2515 and SD card CS pins on SparkFun CAN-Bus Shield
 const int CAN_CS = 10;
+const int SD_CS  = 9;
 MCP_CAN CAN0(CAN_CS);
+File logFile;
 
 // Buffer for incoming frames
 unsigned long canId;
@@ -26,6 +30,22 @@ void setup() {
   }
 
   Serial.println(F("canb0t :: SparkFun RedBoard CAN logger"));
+
+  if (!SD.begin(SD_CS)) {
+    Serial.println(F("SD init failed"));
+    while (1) {
+      delay(1000);
+    }
+  }
+
+  logFile = SD.open("canlog.csv", FILE_WRITE);
+  if (!logFile) {
+    Serial.println(F("Log file open failed"));
+    while (1) {
+      delay(1000);
+    }
+  }
+  logFile.println(F("timestamp_ms,id,dlc,data"));
 
   if (CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
     Serial.println(F("CAN init ok"));
@@ -51,18 +71,7 @@ void sniff() {
   if (CAN0.checkReceive() == CAN_MSGAVAIL) {
     CAN0.readMsgBuf(&len, buf);
     canId = CAN0.getCanId();
-
-    Serial.print(F("ID: 0x"));
-    Serial.print(canId, HEX);
-    Serial.print(F(" DLC:"));
-    Serial.print(len);
-    Serial.print(F(" Data:"));
-    for (int i = 0; i < len; i++) {
-      Serial.print(' ');
-      if (buf[i] < 0x10) Serial.print('0');
-      Serial.print(buf[i], HEX);
-    }
-    Serial.println();
+    logFrame(canId, len, buf);
   }
 }
 
@@ -82,23 +91,44 @@ void sendPid(const unsigned char *data) {
   const unsigned long reqId = 0x7DF;
 
   if (CAN0.sendMsgBuf(reqId, 0, 8, data) == CAN_OK) {
+    logFrame(reqId, 8, data);
     if (CAN0.checkReceive() == CAN_MSGAVAIL) {
       CAN0.readMsgBuf(&len, buf);
       canId = CAN0.getCanId();
-
-      Serial.print(F("RESP 0x"));
-      Serial.print(canId, HEX);
-      Serial.print(F(":"));
-      for (int i = 0; i < len; i++) {
-        Serial.print(' ');
-        if (buf[i] < 0x10) Serial.print('0');
-        Serial.print(buf[i], HEX);
-      }
-      Serial.println();
+      logFrame(canId, len, buf);
     } else {
       Serial.println(F("No response"));
     }
   } else {
     Serial.println(F("PID send failed"));
   }
+}
+
+void logFrame(unsigned long id, byte dlc, byte *data) {
+  Serial.print(F("ID: 0x"));
+  Serial.print(id, HEX);
+  Serial.print(F(" DLC:"));
+  Serial.print(dlc);
+  Serial.print(F(" Data:"));
+  for (int i = 0; i < dlc; i++) {
+    Serial.print(' ');
+    if (data[i] < 0x10) Serial.print('0');
+    Serial.print(data[i], HEX);
+  }
+  Serial.println();
+
+  unsigned long ts = millis();
+  logFile.print(ts);
+  logFile.print(',');
+  logFile.print(id, HEX);
+  logFile.print(',');
+  logFile.print(dlc);
+  logFile.print(',');
+  for (int i = 0; i < dlc; i++) {
+    if (i) logFile.print(' ');
+    if (data[i] < 0x10) logFile.print('0');
+    logFile.print(data[i], HEX);
+  }
+  logFile.println();
+  logFile.flush();
 }
